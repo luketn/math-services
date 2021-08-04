26aaf356a2dd94a649896dfabd720e50accbe226
Luke Thompson <endeavour9@gmail.com>
2021-08-05 08:43:47 +1000

    Fix a bug in the left multiple fraction (copy/paste bug) and add regression tests.

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -44,7 +44,7 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 				statusCode = 400;
 				message = QUERY_PARAMS_MISSING_ERROR;
 			} else {
-				String steps = calculate(left, right);
+				String steps = FractionalRatioSimplifier.calculate(left, right);
 				message = "Calculated successfully!";
 				result = Arrays.asList(steps.split("\n"));
 				statusCode = 200;
@@ -60,76 +60,4 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 				.build();
 	}
 
-
-	public String calculate(String ratioLeftString, String ratioRightString) {
-		Fraction leftFraction = getFraction(ratioLeftString);
-		Fraction rightFraction = getFraction(ratioRightString);
-
-		StringBuilder workingOut = new StringBuilder();
-
-		String initialRatio = formatRatio(leftFraction, rightFraction);
-		LOG.info(initialRatio);
-		workingOut.append(initialRatio).append('\n');
-
-		String secondStep;
-		if (leftFraction.getDenominator() % rightFraction.getDenominator() == 0) {
-			//multiply right numerator by (left denominator/ right denominator)
-			int rightNumerator = rightFraction.getNumerator() * (leftFraction.getDenominator() / rightFraction.getDenominator());
-			leftFraction = getFraction(
-					rightNumerator,
-					rightFraction.getDenominator());
-			secondStep = formatRatio(leftFraction, rightFraction);
-
-		} else if (rightFraction.getDenominator() % leftFraction.getDenominator() == 0) {
-			//multiply left numerator by (right denominator/left denominator)
-			int leftNumerator = leftFraction.getNumerator() * (rightFraction.getDenominator() / leftFraction.getDenominator());
-			leftFraction = getFraction(
-					leftNumerator,
-					rightFraction.getDenominator());
-			secondStep = formatRatio(leftFraction, rightFraction);
-
-		} else {
-			//multiply left and right numerator by their opposing denominator, and set both denominators to the product of the two
-			int leftNumerator = leftFraction.getNumerator() * rightFraction.getDenominator();
-			int rightNumerator = rightFraction.getNumerator() * leftFraction.getDenominator();
-			int denominator = leftFraction.getDenominator() * rightFraction.getDenominator();
-			leftFraction = getFraction(leftNumerator, denominator);
-			rightFraction = getFraction(rightNumerator, denominator);
-			secondStep = formatRatio(leftFraction, rightFraction);
-
-		}
-		workingOut.append(secondStep).append('\n');
-
-		//Now I have two fractions with equal denominators, which can be cancelled out of the ratio
-		int left = leftFraction.getNumerator();
-		int right = rightFraction.getNumerator();
-
-		List<Integer> leftPrimes = Primes.primeFactors(left);
-		List<Integer> rightPrimes = Primes.primeFactors(right);
-
-		for (int leftPointer=0, rightPointer=0; leftPointer < leftPrimes.size() && rightPointer < rightPrimes.size(); ) {
-			Integer leftPrimeValue = leftPrimes.get(leftPointer);
-			Integer rightPrimeValue = rightPrimes.get(rightPointer);
-			if (leftPrimeValue == rightPrimeValue) {
-				Integer primeValue = leftPrimeValue;
-				left /= primeValue;
-				right /= primeValue;
-				leftPointer++;
-				rightPointer++;
-			} else if (leftPrimeValue < rightPrimeValue) {
-				leftPointer++;
-			} else {
-				rightPointer++;
-			}
-		}
-
-		String thirdStep = String.format("%s : %s", left, right);
-		workingOut.append(thirdStep).append('\n');
-
-		return workingOut.toString();
-	}
-
-	private String formatRatio(Fraction fractionOne, Fraction fractionTwo) {
-		return String.format("%s : %s", fractionOne, fractionTwo);
-	}
 }
---
+++ src/test/java/com/serverless/FractionalRatioSimplifierTest.java
@@ -0,0 +1,23 @@
+package com.serverless;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FractionalRatioSimplifierTest {
+
+    @Test
+    public void calculate_equal_right_multiple() {
+        String result = FractionalRatioSimplifier.calculate("4/6", "2/3");
+        assertEquals("4/6 : 2/3\n" +
+                "4/6 : 4/6\n" +
+                "1 : 1\n", result);
+    }
+    @Test
+    public void calculate_equal_left_multiple() {
+        String result = FractionalRatioSimplifier.calculate("2/3","4/6" );
+        assertEquals("2/3 : 4/6\n" +
+                "4/6 : 4/6\n" +
+                "1 : 1\n", result);
+    }
+}
